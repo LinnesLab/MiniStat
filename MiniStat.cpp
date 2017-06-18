05241010609c26f3ff05afb5292e3d594374f234
Orlando Hoilett <orlandohoilett@Orlandos-MacBook-Pro.local>
2017-06-18 11:28:54 -0400

    still lots of changes to make

---
+++ MiniStat.cpp
@@ -0,0 +1,393 @@
+/*
+ * FILENAME:    MiniStat.cpp
+ * AUTHOR:      Orlando S. Hoilett
+ * EMAIL:       ohoilett@purdue.edu
+ * VERSION:     0.0.0.p
+ *
+ *
+ * DISCLAIMER
+ * Copyright (C) Linnes Lab 2016, Purdue University, All rights reserved
+ *
+ */
+
+
+#include "MiniStat.h"
+
+
+//DEFAULT CONSTRUCTOR
+//MiniStat::MiniStat()
+MiniStat::MiniStat()
+{
+}
+
+
+//void MiniStat::initialize()
+//
+//Initializes private objects. This method only needs to be called once in the
+//program and should be called in the setup() function in the Arduino software.
+void MiniStat::initialize()
+{
+    pStat = LMP91000();
+    memory = EEPROM_24C64A();
+    dac = DAC_MCP49xx(DAC_MCP49xx::MCP4922, 10); //int SS_PIN = 10;
+    channel = Mux(channel_Ctrl);
+    
+    pStat.setMENB(pStat_Ctrl);
+}
+
+
+//void MiniStat::begin()
+//
+//Wakes all devices from sleep mode.
+void MiniStat::begin()
+{
+    //wakes up ATmega328
+    sleep_disable();
+    
+    //wakes up LMP91000
+    //pStat.setMENB(pStat_Ctrl);
+    pStat.standby();
+    pStat.disableFET();
+    pStat.setThreeLead();
+    delay(1000);
+    
+    //initiates MCP4922
+    dac.setSPIDivider(SPI_CLOCK_DIV16);
+    dac.setPortWrite(true);
+    dac.setAutomaticallyLatchDual(false);
+    
+}
+
+
+//void MiniStat::sleep()
+//
+//Places all devices in sleep mode for power conservation.
+void MiniStat::sleep()
+{
+    dac.shutdown();
+    pStat.enableFET();
+    pStat.sleep();
+    //set_sleep_mode(SLEEP_MODE_PWR_SAVE); //SLEEP_MODE_PWR_DOWN
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    delay(100);
+    sleep_enable();
+    delay(100);
+    sleep_mode();
+}
+
+
+//void MiniStat::setTarget(uint8_t target) const
+//
+//@param            target: Mux channel to open
+//
+//Sets the target of the potentiostat to the reference electrodes or measurement
+//electrodes.
+void MiniStat::setTarget(uint8_t target) const
+{
+    channel.open(target);
+}
+
+
+//void MiniStat::runRef() const
+//
+//Sets the target of the potentiostat to the reference electrodes.
+void MiniStat::runRef() const
+{
+    setTarget(REFERENCE);
+}
+
+
+//void MiniStat::runExp() const
+//
+//Sets the target of the potentiostat to the measurement electrodes.
+void MiniStat::runExp() const
+{
+    setTarget(EXPERIMENTAL);
+}
+
+
+
+//void MiniStat::runCV(uint8_t scan_rate, int high_limit, int low_limit) const
+//void MiniStat::runCV(double cycles, uint16_t scan_rate)
+//
+//@param            cycles: number of cycles for the CV sweep
+//
+//@param            scan_rate: delay between mesurements
+//
+//Runs cyclic voltammetry protocol. Sets the digital to analog converter to provide
+//a 1/2 Vdd reference voltage. The LMP91000 is then programmed to increment the
+//bias on the reference electrode form 0 to 0.24 times the reference voltage.
+void MiniStat::runCV(uint8_t user_gain, uint8_t cycles, uint16_t scan_rate)
+{
+    dac.outputA(0);
+    dac.outputB(2048);
+    delay(10);
+    
+    pStat.setGain(user_gain);
+    pStat.setRLoad(0);
+    pStat.setExtRefSource();
+    pStat.setIntZ(1);
+    
+    int current = 0;
+    uint8_t index = 0;
+    
+    //for (int i = 0; i < 50; i++) //for acid clean
+    for (int i = 0; i < 2*cycles; i++) //for measurements
+    {
+        int polarity = 0;
+        
+        if (i%2 == 0)
+        {
+            pStat.setNegBias();
+            polarity = -1;
+        }
+        else
+        {
+            pStat.setPosBias();
+            polarity = 1;
+        }
+        
+        for (int j = 1; j <= 13; j++)
+        {
+            method(j, scan_rate, polarity);
+            //Serial.print("j: ");
+            //Serial.println(j);
+        }
+        
+        for (int k = 12; k >= 0; k--)
+        {
+            method(k, scan_rate, polarity);
+            //Serial.print("k: ");
+            //Serial.println(k);
+        }
+    }
+            
+}
+
+
+//void MiniStat::method(uint8_t bias, uint16_t scan_rate, int polarity)
+//
+//
+void MiniStat::method(uint8_t bias, uint16_t scan_rate, int polarity)
+{
+    pStat.setBias(bias);
+    delay(scan_rate);
+    
+    int voltage = polarity*bias_incr[bias]*ADC_REF*1000;
+//    memory.write(memory.getCurReg(), (voltage & 0xFF));
+//    memory.write(memory.getCurReg(), ((voltage >> 8) & 0xFF));
+    
+    uint16_t adcVal = pStat.getOutput(pStat_Sensor);
+    int current = pStat.getCurrent(adcVal, ADC_REF, ADC_BITS)*pow(10,8);
+//    memory.write(memory.getCurReg(), (current & 0xFF));
+//    memory.write(memory.getCurReg(), ((current >> 8) & 0xFF));
+    
+    Serial.print(voltage);
+    Serial.print(",");
+    Serial.print(adcVal);
+    Serial.print(",");
+    Serial.println(-current);
+    //Serial.println(millis());
+
+//    Serial.print(current, DEC);
+//    Serial.print(": ");
+//    Serial.println((current), BIN);
+//    Serial.print(current & 0xFF, BIN);
+//    Serial.print(", ");
+//    Serial.println((current >> 8) & 0xFF, BIN);
+//
+//    Serial.print(voltage, DEC);
+//    Serial.print(": ");
+//    Serial.println((voltage), BIN);
+//    Serial.print(voltage & 0xFF, BIN);
+//    Serial.print(", ");
+//    Serial.println((voltage >> 8) & 0xFF, BIN);
+//    Serial.println();
+//    Serial.println();
+
+    
+//    Serial.print((current & 0xFF), BIN);
+//    Serial.print(", ");
+//    Serial.print(((current >> 8) & 0xFF), BIN);
+//    Serial.print(", ");
+//    Serial.println(current, BIN);
+//    
+//    Serial.print((((int)(polarity*bias_incr[bias]*ADC_REF*1000))&0xFF), BIN);
+//    Serial.print(", ");
+//    Serial.print(((((int)(polarity*bias_incr[bias]*ADC_REF*1000)) >> 8) & 0xFF), BIN);
+//    Serial.print(", ");
+//    Serial.println((((int)(polarity*bias_incr[bias]*ADC_REF*1000))), BIN);
+    
+}
+
+
+void MiniStat::runACV(uint8_t user_gain, uint8_t cycles, uint16_t scan_rate)
+{
+    //pStat.setTwoLead();
+    pStat.setGain(user_gain);
+    pStat.setRLoad(0);
+    pStat.setExtRefSource();
+    pStat.setIntZ(1);
+//    pStat.setPosBias();
+    pStat.setNegBias();
+    pStat.setBias(13);
+    delay(1000); //warm-up time for the gas sensor
+    int current = 0;
+    uint8_t index = 0;
+    
+    
+    unsigned long time0 = millis();
+    unsigned long sample = 100;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    
+    
+    uint32_t sum = 0;
+    
+    while (i > -1)
+    {
+        //Serial.println("hello there!");
+        if((micros() - time0) > 50)
+        {
+            sum += analogRead(pStat_Sensor);
+            i++;
+            time0 = micros();
+        
+            if (i%(12-1) == 0)
+            {
+                double data = waveformsTable[sineWave][j];
+                data = ((data - 2048.0)/25.0) + 2048.0;
+                dac.outputB(data);
+                j++;
+            }
+            if (i%(120-1) == 0)
+            {
+                double data = waveformsTable[triWave][k];
+                data = ((data - 2048.0)*.8) + 2048.0;
+                //data = ((data - 2048.0)*.6) + 1248.0;
+                dac.outputA(data);
+                k++;
+            }
+            
+            if(j == maxSamplesNum-1) j = 0;
+            if(k == maxSamplesNum-1)
+            {
+                k = 0;
+               // i = 0;
+            }
+            
+            //average(sum / max_samples);
+            //current = getCurrent(pStat_Sensor, ADC_REF, ADC_BITS)*pow(10,8);
+            //current = getCurrent(average, ADC_REF, ADC_BITS)*pow(10,8);
+//            memory.write(memory.getCurReg(), (current & 0xFF));
+//            memory.write(memory.getCurReg(), ((current >> 8) & 0xFF));
+//            memory.write(memory.getCurReg(), (((int)(polarity*bias_incr[bias]*ADC_REF*1000))&0xFF));
+//            memory.write(memory.getCurReg(), ((((int)(polarity*bias_incr[bias]*ADC_REF*1000)) >> 8) & 0xFF));
+        }
+    }
+    
+}
+
+
+void MiniStat::runPulseV(uint8_t user_gain, uint8_t cycles, uint16_t scan_rate)
+{
+    
+}
+
+
+//void MiniStat::print()
+//
+//Reads the voltage and current data from memory storage and prints to the
+//serial monitor.
+void MiniStat::print()
+{
+    for (int i = 0; i < 256; i=i+4)
+    {
+        int voltage = (int)((memory.read(i+1) << 8) | memory.read(i));
+        int current = (int)((memory.read(i+3) << 8) | memory.read(i+2));
+        
+        Serial.print(i); //for debug purposes
+        Serial.print(","); //for debug purposes
+        Serial.print(voltage);
+        Serial.print(",");
+        Serial.println(current);
+    }
+}
+
+
+/*
+ Convert to current
+ 
+ set parameters for LMP91000
+ 
+ set parameters for MPC4922
+ */
+
+
+/*
+ *
+ * MiniStat program structure
+ *
+ *
+ 
+ volatile unsigned long counter = 0;
+ const unsigned long 2hrs = ___;
+ 
+ //500 Hz
+ TCCR2A = 0;
+ TCCR2B = 0;
+ TCNT2  = 0;
+ OCR2A = 249;// = (8*10^6) / (500*64) - 1 (must be <256)
+ TCCR2A |= (1 << WGM21);
+ TCCR2B |= (1 << CS22);
+ TIMSK2 |= (1 << OCIE2A);
+ 
+ 
+ ISR(TIMER2_COMPA_vect)
+ {
+ counter++;
+ if(counter > 2hrs)
+ {
+ sleep_disable();
+ counter = 0;
+ }
+ }
+ 
+ 
+every two hours
+{
+    1. Wake up from sleep
+    a. Wake up microcontroller        sleep_disable();
+    b. Wake LMP91000                  LMP91000.setThreeLead(); LMP91000.disableFET();
+    c. Give LMP91000 time to warm up  delay(2000);
+    d. Wake up MCP4922                dac.output();
+    2. Measure reference
+    a. Set muxes to reference         mux.open(reference);
+    b. CV, ACV or Pulse V protocol    LMP91000.runTest();
+    3. Store data                       myEEPROM.write(voltage); myEEPROM.write(current);
+    
+    myEEPROM.write(myEEPROM.getCurReg()+1, (int)(1*bias_incr[i]*3.3*100));
+    
+    4. Measure experimental
+    a. Set muxes to experimental      mux.open(experimental);
+    b. CV, ACV or Pulse V protocol    LMP91000.runTest();
+    5. Store data                       myEEPROM.write(voltage); myEEPROM.write(current);
+    6. Run statistical analysis         runTtest();
+    7. Stores result                    myEEPROM.write(result); myEEPROM.write(p-value);
+    8. Go back to sleep
+    a. Shut down MCP4922              dac.shutdown();
+    b. Shutdown LMP91000              LMP91000.enableFET(); LMP91000sleep();
+    c. Shutdown ATmega                set_sleep_mode(SLEEP_MODE_PWR_SAVE);
+}
+
+
+How to handle serial connection
+
+
+Sending data over Bluetooth
+*
+*
+*/
+
+
---
+++ MiniStat.h
@@ -0,0 +1,103 @@
+/*
+ * FILENAME:    MiniStat.h
+ * AUTHOR:      Orlando S. Hoilett
+ * EMAIL:       ohoilett@purdue.edu
+ * VERSION:     0.0.0.p
+ 
+ 
+ * DESCRIPTION
+ 
+ 
+ 
+ * A FEW INSTRUCTIONS
+ 
+ 
+ * UPDATES
+ * Version 0.0
+ * 2015/02/11:2220>
+ *			Initialization of code development.
+
+ 
+ * DISCLAIMER
+ * Copyright (C) Linnes Lab 2016, Purdue University, All rights reserved
+ *
+ */
+
+
+#ifndef MINISTAT_H
+#define MINISTAT_H
+
+#include "Arduino.h"
+#include "Wire.h"
+#include "SPI.h"
+
+#include "avr/interrupt.h"
+#include "avr/power.h"
+#include "avr/sleep.h"
+#include "avr/io.h"
+
+#include "LMP91000.h"
+#include "EEPROM_24C64A.h"
+#include "DAC_MCP49xx.h"
+#include "Waveforms.h"
+#include "Mux.h"
+
+
+
+const uint8_t pStat_Ctrl= 4; //Arduino Digital Pin 4
+const uint8_t pStat_Sensor = A0; //Arduino Analog In pin 0
+const uint8_t channel_Ctrl = 3; //Arduino Digital Pin 3
+
+//uint16_t scan_rate = 50;
+//uint8_t gain = 3; //for acid clean
+//uint8_t tiaGain = 6; //for measurements
+const double gain_resistors[] = {2750,3500,7000,14000,35000,120000,350000};
+const int sensor = A0;
+const double bias_incr[] = {0, 0.01, 0.02, 0.04, 0.06, 0.08,
+    0.1,0.12, 0.14, 0.16, 0.18, 0.2, 0.22, 0.24};
+
+const uint8_t REFERENCE = 0;
+const uint8_t EXPERIMENTAL = 1;
+
+const double ADC_REF = 3.36;
+const double ADC_BITS = 10;
+
+const uint8_t sineWave = 0;
+const uint8_t triWave = 1;
+
+
+class MiniStat {
+    
+private:
+    LMP91000 pStat;
+    EEPROM_24C64A memory;
+    DAC_MCP49xx dac;
+    Mux channel;
+    
+    
+public:
+    
+    //CONSTRUCTORS
+    MiniStat();
+    
+    void initialize();
+    void begin();
+    void sleep();
+    
+    void setTarget(uint8_t target) const;
+
+    void runRef() const; //runRef
+    void runExp() const; //runExp
+    
+    //void runCV(double cycles, uint16_t scan_rate); //for debug purposes
+    void runCV(uint8_t user_gain, uint8_t cycles, uint16_t scan_rate);
+    void runACV(uint8_t user_gain, uint8_t cycles, uint16_t scan_rate);
+    void runPulseV(uint8_t user_gain, uint8_t cycles, uint16_t scan_rate);
+    void method(uint8_t bias, uint16_t scan_rate, int polarity);
+
+    void save();
+    void print();
+    
+};
+
+#endif
